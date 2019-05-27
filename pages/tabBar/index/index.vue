6c835e389f79ffdf3d2fd2660fb3baec7bfcc826
476602836 <476602836@qq.com>
2019-05-27 18:19:03 +0800

    增加我的页面

--- pages/tabBar/index/index.vue
+++ pages/tabBar/index/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<view>
 		<!-- <page-head title="view"></page-head> -->
-		<uni-swiper-dot :info="info" :current="current" :mode="mode" :dots-styles="dotsStyles" field="content">
-			<swiper class="swiper-box" @change="change">
+		<uni-swiper-dot :info="info" :current="current" :mode="mode1" :dots-styles="dotsStyles" field="content">
+			<swiper class="swiper-box" @change="change" autoplay="true" >
 				<swiper-item v-for="(item ,index) in info" :key="index">
 					<view :class="item.colorClass" class="swiper-item">
 						<image :src="item.url" mode="aspectFill" />
@@ -14,19 +14,20 @@
 		<view class="" style="text-align: center;font-size: 20px;color: #000000;">输入17位VIN码</view>
 		<view class="container">
 			<view class="code-slide code-slide1">
-				<view class="uni-icon uni-icon-scan" style="color: rgb(143, 143, 148); font-size: 25px;"></view>
+				<view class="uni-icon uni-icon-scan" style="color: rgb(143, 143, 148); font-size: 25px;" @tap="chooseImage"></view>
 			</view>
 			<view class="code-slide code-slide2">
 				<view class="uni-form-item uni-column">
-					<input class="uni-input" focus placeholder="输入17位VIN码" style="background: #f7f3f3;"/>
+					<input class="uni-input" v-model="vin" @input="onKeyVinInput" maxlength="17"  focus placeholder="输入17位VIN码" style="background: #f7f3f3;"/>
 				</view>
 			</view>
 			<view class="code-slide code-slide3">
-				<view  class="uni-icon uni-icon-info-filled" style="color: rgb(143, 143, 148); font-size: 25px;"></view>
+				<view  class="uni-icon uni-icon-info-filled" style="color: rgb(143, 143, 148); font-size: 25px;" @tap="actionSheetTap"></view>
 			</view>
 		</view>
 		<view style="clear:both;"></view>
-		<button type="primary" style="width: 95%;">立即查询</button>
+		<button type="primary"  @click="showMulLinkageTwoPicker"  style="width: 95%;margin-top: 5upx;background-color: #ff9600;">{{pickerText}}</button>
+		<button @tap="search" type="primary" style="width: 95%;margin-top: 5upx;"  >立即查询</button>
 		
 		<view class="uni-common-mt" style="background:#FFF; padding:20upx;">
 			<rich-text :nodes="strings"></rich-text>
@@ -39,15 +40,35 @@
 			</view>
 
 		</view>
+		<mpvue-picker :themeColor="themeColor" ref="mpvuePicker" :mode="mode" :deepLength="deepLength" :pickerValueDefault="pickerValueDefault"
+		 @onConfirm="onConfirm" @onCancel="onCancel" :pickerValueArray="pickerValueArray"></mpvue-picker>
 	</view>
+	
 </template>
 <script>
 	import uniSwiperDot from '@/components/uni-swiper-dot/uni-swiper-dot.vue'
 	import uLink from "@/components/uLink.vue"
+	import mpvuePicker from '../../../components/mpvue-picker/mpvuePicker.vue';
+	// https://github.com/zhetengbiji/mpvue-citypicker
+	import mpvueCityPicker from '../../../components/mpvue-citypicker/mpvueCityPicker.vue'
+	import cityData from '../../../common/car.data.js';
+	import jweixin from '../../../common/wxpay.js';
+	var sourceType = [
+		['camera'],
+		['album'],
+		['camera', 'album']
+	]
+	var sizeType = [
+		['compressed'],
+		['original'],
+		['compressed', 'original']
+	]
 	export default {
 		components: {
 			uniSwiperDot,
-			uLink
+			uLink,
+			mpvuePicker,
+			mpvueCityPicker
 		},
 		data() {
 			return {
@@ -92,7 +113,7 @@
 				modeIndex: -1,
 				styleIndex: -1,
 				current: 0,
-				mode: 'default',
+				mode1: 'default',
 				dotsStyles: {},
 				focus: false,
 				inputValue: '',
@@ -103,11 +124,115 @@
 				src: '../../../static/eye-1.png',
 				platform: '',
 				title: 'input',
-				strings: '<div style="text-align:center;background:#d4f3f2;color: #607D8B;"><h2>通知</h2><div style="width:90%;display:inline-block;text-align: left;">由于查询人数较多，现在查询价格暂定，后期价格会略有浮动</div></div>'
+				strings: '<div style="text-align:center;background:#d4f3f2;color: #607D8B;"><h2>通知</h2><div style="width:90%;display:inline-block;text-align: left;">由于查询人数较多，现在查询价格暂定，后期价格会略有浮动</div></div>',
+				vin:'',
+				// 选择车型
+				mulLinkageTwoPicker: cityData,
+				cityPickerValueDefault: [0, 0, 1],
+				themeColor: '#007AFF',
+				pickerText: '选择车型',
+				mode: '',
+				deepLength: 1,
+				pickerValueDefault: [0],
+				pickerValueArray:[],
+				
+				// 上传图片
+				imageList: [],
+				sourceTypeIndex: 2,
+				sourceType: ['拍照', '相册', '拍照或相册'],
+				sizeTypeIndex: 2,
+				sizeType: ['压缩', '原图', '压缩或原图'],
+				countIndex: 8,
+				count: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+				
+				vins : ''
+				
 			}
 		},
-		onLoad() {},
+		// onLoad() {
+		// 	
+		// 	
+		// },
+		onUnload() {
+			this.imageList = [],
+				this.sourceTypeIndex = 2,
+				this.sourceType = ['拍照', '相册', '拍照或相册'],
+				this.sizeTypeIndex = 2,
+				this.sizeType = ['压缩', '原图', '压缩或原图'],
+				this.countIndex = 8;
+		},
 		methods: {
+			actionSheetTap() {
+				var vins = uni.getStorageSync("vins")
+				var itemList = vins.split(",").reverse() 
+				uni.showActionSheet({
+					title:'最近查询',
+					itemList: itemList,
+					success: (e) => {
+						console.log(e.tapIndex);
+						// uni.showToast({
+						// 	title:"点击了第" + e.tapIndex + "个选项",
+						// 	icon:"none"
+						// })
+						this.vin = itemList[e.tapIndex]
+					}
+				})
+			},
+			// 二级联动选择
+			showMulLinkageTwoPicker() {
+				this.pickerValueArray = this.mulLinkageTwoPicker
+				this.mode = 'multiLinkageSelector'
+				this.deepLength = 2
+				this.pickerValueDefault = [0, 0]
+				this.$refs.mpvuePicker.show()
+			},
+			onConfirm(e) {
+				this.pickerText = e.label
+			},
+			
+			onKeyVinInput: function(event) {  
+                this.vin = event.target.value
+				//console.log(this.vin)
+            },  
+			search(){
+				if(this.vin.length!=17){
+					uni.showToast({
+						title: "请输入正确的VIN码",
+						icon: "info-filled",
+						duration: 1000
+					})
+					return;
+				}
+				jweixin.ready(function(){  
+					console.log(1)
+				});
+				//uni.setStorageSync('vins','');
+				if(-1==uni.getStorageSync('vins').indexOf(this.vin))uni.setStorageSync('vins',uni.getStorageSync('vins')+','+this.vin);
+				console.log(uni.getStorageSync('vins').indexOf(this.vin))
+				uni.navigateTo({
+					url: 'search/search?data='+this.vin
+				})
+			},
+			chooseImage: async function() {
+					uni.chooseImage({
+						success: (chooseImageRes) => {
+							const tempFilePaths = chooseImageRes.tempFilePaths;
+							uni.uploadFile({
+								url: 'http://weiqing.zxiu58.top/app/index.php?i=3&c=entry&op=index&do=Upload2&m=ab_qrcode', //仅为示例，非真实的接口地址
+								filePath: tempFilePaths[0],
+								name: 'file',
+								formData: {
+									'user': 'test'
+								},
+								success: (uploadFileRes) => {
+									uploadFileRes.data = "2GFD5G5G4HG45GH78"
+									console.log(uploadFileRes.data);
+									this.vin = uploadFileRes.data
+								}
+							});
+						}
+					});
+				},
 			change(e) {
 				this.current = e.detail.current
 			},
@@ -161,7 +286,18 @@
 				})
 			}
 		},
+		
 		onLoad() {
+			// try {
+			// 	this.vins = uni.getStorageSync('vins');
+			// 	if (this.vins) {
+			// 		console.log(this.vins);
+			// 	}else{
+			// 		uni.setStorageSync('vins', []);
+			// 	}
+			// } catch (e) {
+			// 	// error
+			// }
 			this.platform = uni.getSystemInfoSync().platform
 		}
 
---
+++ pages/tabBar/my/my.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+	<view >
+		<view class="uni-center" style="background:#FFFFFF; font-size:0;">
+			<image class="image" mode="widthFix" src="https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png" />
+		</view>
+	</view>
+	<uni-list>
+		<uni-list-item title="我的查询" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/hx.png" />
+		<uni-list-item title="我的车辆" note="" thumb="https://img-cdn-qiniu.dcloud.net.cn/new-page/uni.png" />
+	</uni-list>
+	</view>
+</template>
+
+<script>
+	import uniList from '@/components/uni-list/uni-list.vue'
+	import uniListItem from '@/components/uni-list-item/uni-list-item.vue'
+	
+	export default {
+		components: {
+			uniList,
+			uniListItem
+		},
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.image {
+		margin:40upx 0;
+		width: 200upx;
+	}
+</style>
